62195476347a3803b3f52721247cdeb8af6afebc
monksbronks <116058964+MonkaSztuder@users.noreply.github.com>
2023-06-09 13:28:03 +0200

    wk

--- watek_komunikacyjny.cpp
+++ watek_komunikacyjny.cpp
@@ -7,39 +7,40 @@ void *startKomWatek(void *ptr)
     int is_message = FALSE;
     packet_t pakiet;
     int lamport_earlier;
-   // / Obrazuje pętlę odbierającą pakiety o różnych typach /
-    while ( stan!=InFinish ) {
-    debug("czekam na recv");
-        MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    // / Obrazuje pętlę odbierającą pakiety o różnych typach /
+    while (stan != InFinish)
+    {
+        debug("czekam na recv");
+        MPI_Recv(&pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         lamport_earlier = lamport;
-        if(pakiet.ts >lamport)
+        if (pakiet.ts > lamport)
         {
-            lamport=pakiet.ts;
+            lamport = pakiet.ts;
         }
         lamport++;
 
-        switch ( status.MPI_TAG ) {
-        case REQUEST: 
-                debug("Ktoś coś prosi. A niech ma!")
-        if(stan == InRun || (stan == InWant && pakiet.ts <= lamport_earlier))
+        switch (status.MPI_TAG)
         {
-            if(pakiet.ts == lamport_earlier && status.MPI_SOURCE > rank)
-                break;
-            printf("%d Dostałem REQ od %d, odesłałem\n", rank,status.MPI_SOURCE);
-            sendPacket( 0, status.MPI_SOURCE, ACK );
-        }
-        break;
-        case ACK: 
-                debug("Dostałem ACK od %d, mam już %d", status.MPI_SOURCE, ackCount);
+        watek_komunikacyjny case REQUEST:
+            debug("Ktoś coś prosi. A niech ma!") if (stan == InRun || (stan == InWant && pakiet.ts <= lamport_earlier))
+            {
+                if (pakiet.ts == lamport_earlier && status.MPI_SOURCE > rank)
+                    break;
+                printf("%d Dostałem REQ od %d, odesłałem\n", rank, status.MPI_SOURCE);
+                sendPacket(0, status.MPI_SOURCE, ACK);
+            }
+            break;
+        case ACK:
+            debug("Dostałem ACK od %d, mam już %d", status.MPI_SOURCE, ackCount);
             ackCount++; // czy potrzeba tutaj muteksa? Będzie wyścig, czy nie będzie? Zastanówcie się. /
-        break;
+            break;
         case RELEASE:
-                debug("Dostałem RELEASE od %d, mam już %d", status.MPI_SOURCE, ackCount);
+            debug("Dostałem RELEASE od %d, mam już %d", status.MPI_SOURCE, ackCount);
             ackCount++; // czy potrzeba tutaj muteksa? Będzie wyścig, czy nie będzie? Zastanówcie się. */
 
-        break;
+            break;
         default:
-        break;
+            break;
         }
     }
 }
