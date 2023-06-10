66abac3cefd314feb7ba3a5f1d4c6d8cb94474a8
MonkaSztuder <116058964+MonkaSztuder@users.noreply.github.com>
2023-06-10 15:13:57 +0200

    skanseny

---
+++ skanseny.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "skanseny.h"
+
+void generuj_zlecenia(int id_skansenu)
+{
+    int ile=random()%10;
+    
+}
---
+++ skanseny.h
@@ -0,0 +1,4 @@
+#pragma once
+#define SKANSENY_H
+
+void generuj_zlecenia(int id_skansenu);
